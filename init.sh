366aab772636195f2fd422b426c9d1ea45d86685
Levy-Hara <ml185239@ncr.com>
2019-07-10 11:47:47 -0400

    init

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-docker-compose up --build -d
-
 #build frontend
 cd frontend
 npm run build
@@ -9,3 +7,6 @@ npm run build
 #move build to auth container
 mkdir ../auth/build_client
 mv build ../auth/build_client
+
+#start containers
+docker-compose up --build -d
