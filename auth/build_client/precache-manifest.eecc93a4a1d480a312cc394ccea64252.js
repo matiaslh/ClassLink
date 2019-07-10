5daea43cabba770968c374411914339159be23c2
Matias Levy-Hara <matiaslh@gmail.com>
2019-07-10 14:48:00 +0000

    bs

---
+++ auth/build_client/precache-manifest.eecc93a4a1d480a312cc394ccea64252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7475e466d0a4167006c1bbaa7a1a77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "162c7862f09091636459",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "114de2ebf67720041a99",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "162c7862f09091636459",
+    "url": "/static/js/2.1f456ecd.chunk.js"
+  },
+  {
+    "revision": "114de2ebf67720041a99",
+    "url": "/static/js/main.acf8b791.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
