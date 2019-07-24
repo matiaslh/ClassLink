59652b4ea653ad3e85a0c935d4d3d45d5b1d02c8
Levy-Hara <ml185239@ncr.com>
2019-07-24 11:59:14 -0400

    courses

---
+++ frontend/src/components/screens/Scheduler.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { withRouter } from "react-router-dom"
+
+class Scheduler extends React.Component {
+
+    state = {
+        courses:[
+            {}
+        ]
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default withRouter(Notify)
+
+const styles = {
+
+}
