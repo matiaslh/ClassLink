ccea32fccb1857422777e22d68435970feabd0b8
Levy-Hara <ml185239@ncr.com>
2019-08-16 16:02:44 -0400

    clojure

--- scheduler/src/scheduler/core.cljs
+++ scheduler/src/scheduler/core.cljs
@@ -5,37 +5,42 @@
 
 (defn get-day-from-time
   "Creates day using the time object provided"
-  [time]
-  {(.-day time) [time]}
+  [time section]
+  (def elem (assoc (js->clj time) :section (dissoc (js->clj section) "meetingInformation")))
+  {(get time "day") [elem]}
 )
 
-(defn ^:export create-schedule-with-section
+(defn create-schedule-with-section
   "Creates a schedule using the section provided"
   [section]
-  (clj->js (apply merge-with into (map get-day-from-time (.-times (.-meetingInformation section)))))
+  (apply merge-with into (for [time (get-in section ["meetingInformation" "times"])] (get-day-from-time time section)))
 )
 
-(defn ^:export get-new-schedules-with-sections
-  "Gets a new section and previous schedules as input and returns new schedules with that section added"
-  [newSections, schedules]
+(defn merge-schedule-lists
+  "Merges two schedule lists and returns concatenated schedule list"
+  [schedule-list-X schedule-list-Y]
+  (into []
+    (for [x schedule-list-X y schedule-list-Y]
+      (merge-with
+        (fn [time-a time-b]
+          (js/console.log (clj->js time-a) (clj->js time-b))
+
+          (if
+            (or (> (get time-a "start") (get time-b "end")) (< (get time-a "end") (get time-b "start"))) (merge-with into time-a time-b)
+
+          ))
+        x y)
+    ))
+)
+
+(defn get-new-schedules-with-sections
+  "Gets a new section and previous schedule list as input and returns new schedules with that section added"
+  [new-sections old-schedule-list-js]
+  (def new-schedule-list (map create-schedule-with-section (js->clj new-sections)))
+  (def old-schedule-list (js->clj old-schedule-list-js))
   (cond
-    (empty? schedules) (clj->js (map create-schedule-with-section newSections))
+    (empty? old-schedule-list) (clj->js new-schedule-list)
+    :else (clj->js (merge-schedule-lists old-schedule-list new-schedule-list))
   )
 )
 
-(js/console.log (clj->js (apply merge-with into
-	  [{"Lisp" [1,2,3]
-	   "ML" [10,11,12]}
-	  {"Lisp" [4,5]
-	   "ML" [13,14]}] )))
-
-;; define your app data so that it doesn't get over-written on reload
-
-(defonce app-state (atom {:text "Hello world!"}))
-
-
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
